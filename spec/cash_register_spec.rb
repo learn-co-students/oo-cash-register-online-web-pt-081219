be996b50a0cc9972bab74c8041fb1f1d7cc6cbbb
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-25 21:08:26 +0000

    Done.

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -76,7 +76,7 @@ describe 'CashRegister' do
 
   describe '#void_last_transaction' do
     it 'subtracts the last item from the total' do
-      cash_register.add_item("apple", 0.99)
+      cash_register.add_item("apple", '0.99)
       cash_register.add_item("tomato", 1.76)
       cash_register.void_last_transaction
       expect(cash_register.total).to eq(0.99)
